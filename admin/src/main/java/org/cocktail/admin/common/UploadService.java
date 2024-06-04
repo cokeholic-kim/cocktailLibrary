34a7a30383283ffdfa99f3a412ce63ff55a65f4d
dong_hy <kimadonghyon99@gmail.com>
2024-06-04 21:10:59 +0900

    feat : 파일 업로드서비스 수정 1. 삭제 기능추가 2. 업데이트기능 추가 (기존파일이름, 새로들어온파일) 3. 파일랜덤이름 생성 static으로 설정

--- admin/src/main/java/org/cocktail/admin/common/UploadService.java
+++ admin/src/main/java/org/cocktail/admin/common/UploadService.java
@@ -2,6 +2,7 @@ package org.cocktail.admin.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,18 +12,45 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @Slf4j
 public class UploadService {
-//    @Value("${file.add}")
-//    public static String UPLOAD_PATH = System.getProperty("user.dir") +"\\admin\\src\\main\\resources\\static\\imgs";
-//    public static String UPLOAD_PATH = System.getProperty("user.dir") +"\\admin\\src\\main\\webapp\\imgs\\";
 
     //외부폴더
     public static String UPLOAD_PATH = "file:///C:/Users/kimad/Desktop/JavaPortpolio/coctail_library_imgs/";
     private String uploadPath = "C:/Users/kimad/Desktop/JavaPortpolio/coctail_library_imgs/";
-    public void saveFile(MultipartFile file, String fileName) {
+
+    public static String createFileName(MultipartFile file){
+        return UUID.randomUUID() + "_" + file.getOriginalFilename();
+    }
+
+    public void saveFile(MultipartFile file) {
         try {
-            file.transferTo(new File(uploadPath, fileName));
+            file.transferTo(new File(uploadPath, createFileName(file)));
         } catch (IOException e) {
             log.error("Error occurred while saving file: {}", e.getMessage(), e);
+        }
+    }
+
+    // 파일 삭제 메서드
+    public void deleteFile(String fileName) {
+        try {
+            File file = new File(uploadPath, fileName);
+            if (file.delete()) {
+                log.info("{} 파일 삭제 성공", fileName);
+            } else {
+                log.warn("{} 파일 삭제 실패", fileName);
+            }
+        } catch (Exception e) {
+            log.error("Error occurred while deleting file: {}", e.getMessage(), e);
+        }
+    }
+
+    public void updateFile(String oldFileName,MultipartFile newFile){
+        if(!newFile.isEmpty()){
+            //새로운 이미지가 업데이트 되는경우
+
+            //기존이미지를 삭제
+            deleteFile(oldFileName);
+            //새로운 이미지를 추가
+            saveFile(newFile);
         }
     }
 }
