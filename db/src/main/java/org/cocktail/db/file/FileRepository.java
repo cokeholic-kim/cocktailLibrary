0baac0852f3ef5e16dbe05889caadd16e9908bcc
dong_hy <kimadonghyon99@gmail.com>
2024-06-26 19:00:14 +0900

    feat : fileEntity 생성 1. 파일저장시 파일접근 경로를 각 테이블에서 가지고있던것을 분리. 2. 파일테이블 생성. 3. 파일 이름,접근경로, 용량 ,크기,타입,생성일시,업로더 정보를 저장.

---
+++ db/src/main/java/org/cocktail/db/file/FileRepository.java
@@ -0,0 +1,8 @@
+package org.cocktail.db.file;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<FileEntity,Long> {
+    Optional<FileEntity> findByFileName(String fileName);
+}
