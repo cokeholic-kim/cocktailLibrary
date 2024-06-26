dcfdbb9a746065a2ec62cc323dfc12e39dc7ff3e
dong_hy <kimadonghyon99@gmail.com>
2024-06-26 19:21:00 +0900

    feat : 재료전송 메소드 수정. 1. 삭제요청시 파라미터 추가, 삭제할 entity의 이름과 파일의 이름을 받도록 수정.

--- admin/src/main/resources/static/js/Ingredient.js
+++ admin/src/main/resources/static/js/Ingredient.js
@@ -1,5 +1,9 @@
 const onDeleteIngredient = (e) => {
-    fetch(`/ingredient/delete/${e.target.parentElement.dataset.id}`)
+    const name = e.target.parentElement.dataset.name;
+    const filename = e.target.parentElement.dataset.filename;
+
+    fetch(`/ingredient/delete?name=${name}&filename=${filename}`
+        )
         .then(data => {
             // 응답 데이터 처리
             console.log(data);
@@ -19,7 +23,7 @@ document.querySelectorAll(".ingredientDelete").forEach(item => {
 
 document.querySelectorAll(".openIngredientDetail").forEach(item => {
     item.addEventListener('click', (e) => {
-        let url = `/ingredient/detail/${e.target.parentElement.dataset.id}`
+        let url = `/ingredient/detail/${e.target.parentElement.dataset.name}`
         let option = "width = 500, height = 500, top = 100, left = 200, location = no"
         window.open(url, e.target.innerText, option)
     })
