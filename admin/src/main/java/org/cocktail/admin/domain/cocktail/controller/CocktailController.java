40ce0d7bc361008475d1ae5db85d2c2577338582
dong_hy <kimadonghyon99@gmail.com>
2024-06-30 19:23:09 +0900

    feat : CocktailController.java 1.칵테일페이지에 모델추가 단위,재료정보

--- admin/src/main/java/org/cocktail/admin/domain/cocktail/controller/CocktailController.java
+++ admin/src/main/java/org/cocktail/admin/domain/cocktail/controller/CocktailController.java
@@ -1,23 +1,32 @@
 package org.cocktail.admin.domain.cocktail.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.cocktail.admin.domain.Ingredient.business.IngredientBusiness;
+import org.cocktail.admin.domain.Ingredient.controller.model.IngredientResponse;
 import org.cocktail.admin.domain.cocktail.business.CocktailBusiness;
 import org.cocktail.admin.domain.cocktail.controller.model.CockTailRequest;
 import org.cocktail.admin.domain.cocktail.controller.model.CockTailUpdateRequest;
-import org.cocktail.admin.domain.cocktail.controller.model.CocktailRequests;
+import org.cocktail.admin.domain.cocktail.controller.model.CocktailIngredientRequest;
+import org.cocktail.admin.domain.cocktail.controller.model.CocktailIngredientResponse;
+import org.cocktail.admin.domain.cocktail.controller.model.CocktailResponse;
+import org.cocktail.db.CocktailIngredient.CocktailIngredientEntity;
+import org.cocktail.db.CocktailIngredient.enums.Unit;
 import org.cocktail.db.cocktail.CocktailEntity;
 import org.cocktail.db.cocktail.enums.Glass;
 import org.cocktail.db.cocktail.enums.Method;
-import org.springframework.boot.Banner.Mode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,42 +40,50 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CocktailController {
 
     private final CocktailBusiness cocktailBusiness;
+    private final IngredientBusiness ingredientBusiness;
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/page")
-    public String cocktailPage(Model model){
+    public String cocktailPage(Model model) {
         List<CocktailEntity> allCocktail = cocktailBusiness.getAllCocktail();
+        List<IngredientResponse> ingredientResponses = ingredientBusiness.readAll();
         model.addAttribute("allCocktail", allCocktail);
+        model.addAttribute("allIngredients", ingredientResponses);
         model.addAttribute("glasses", Glass.values());
         model.addAttribute("methods", Method.values());
+        model.addAttribute("units", Unit.values());
 
         return "cocktail/index";
     }
+
     @PostMapping("/register")
-    public String register(CockTailRequest request) throws IOException {
+    public String register(
+            @ModelAttribute CockTailRequest cockTailRequest
+    ) throws IOException {
         String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-        cocktailBusiness.register(request,userName);
-        return "redirect:/";
+        cocktailBusiness.register(cockTailRequest, userName);
+        return "redirect:/cocktail/index";
     }
 
     @GetMapping("/delete")
     @ResponseBody
-    public ResponseEntity<Void> delete(@RequestParam String name, @RequestParam Long id){
-        cocktailBusiness.deleteCocktail(id,name);
+    public ResponseEntity<Void> delete(@RequestParam String name, @RequestParam Long id) {
+        cocktailBusiness.deleteCocktail(id, name);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/Detail/{id}")
-    public String Detail(@PathVariable Long id,Model model){
-        CocktailEntity cocktail = cocktailBusiness.findCocktail(id);
-        model.addAttribute("cocktail",cocktail);
+    public String Detail(@PathVariable Long id, Model model) {
+        CocktailResponse cocktail = cocktailBusiness.findCocktail(id);
+        model.addAttribute("cocktail", cocktail);
         return "cocktail/cockTailDetail";
     }
 
     @PostMapping("/update")
     @ResponseBody
-    public String update( CockTailUpdateRequest request) throws IOException {
+    public String update(CockTailUpdateRequest request) throws IOException {
         String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-        cocktailBusiness.updateCocktail(request,userName);
+        cocktailBusiness.updateCocktail(request, userName);
 
         return "redirect:/cocktail/index";
     }
