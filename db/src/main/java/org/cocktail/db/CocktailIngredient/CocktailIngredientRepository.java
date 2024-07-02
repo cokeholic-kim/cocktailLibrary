213b2d7774454bcbdf0a0bcd634186961f1cd836
dong_hy <kimadonghyon99@gmail.com>
2024-07-02 19:41:02 +0900

    refactor : CocktailIngredientRepository.java
    1. 해당하는 칵테일에 등록된 재료를 삭제하는 메서드추가.

--- db/src/main/java/org/cocktail/db/CocktailIngredient/CocktailIngredientRepository.java
+++ db/src/main/java/org/cocktail/db/CocktailIngredient/CocktailIngredientRepository.java
@@ -1,6 +1,8 @@
 package org.cocktail.db.CocktailIngredient;
 
+import org.cocktail.db.cocktail.CocktailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CocktailIngredientRepository extends JpaRepository<CocktailIngredientEntity,Long> {
+    void deleteAllByCocktail(CocktailEntity cocktail);
 }
