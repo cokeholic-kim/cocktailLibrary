27d63ca8bf4c47d25f7e01a51d4d2c8ea089694d
dong_hy <kimadonghyon99@gmail.com>
2024-07-02 19:35:00 +0900

    refactor : CockTailUpdateRequest.java
    1.필드추가
    2.추가된 배열필드를 CocktailIngredientRequest 객체로 변경하는 메서드 추가.

--- admin/src/main/java/org/cocktail/admin/domain/cocktail/controller/model/CockTailUpdateRequest.java
+++ admin/src/main/java/org/cocktail/admin/domain/cocktail/controller/model/CockTailUpdateRequest.java
@@ -1,5 +1,7 @@
 package org.cocktail.admin.domain.cocktail.controller.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Data;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,10 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 @Getter
 public class CockTailUpdateRequest {
+    //TODO 예외처리 id나 특정값은 받지않게 수정.
     private Long cockTailId;
     private String userId;
     private String cocktailName;
-    private String ingredients;
     private double proof;
     private String glass;
     private String method;
@@ -18,4 +20,20 @@ public class CockTailUpdateRequest {
     private MultipartFile image;
     private String description;
     private String existFileName;
+    private List<String> ingredientName;
+    private List<Double> ingredientVolume;
+    private List<String> ingredientUnit;
+
+    public List<CocktailIngredientRequest> getListIngredients(){
+        ArrayList<CocktailIngredientRequest> cocktailIngredientRequests = new ArrayList<CocktailIngredientRequest>();
+        for (int i = 0; i < ingredientName.size(); i++) {
+            CocktailIngredientRequest build = CocktailIngredientRequest.builder()
+                    .name(this.ingredientName.get(i))
+                    .volume(this.ingredientVolume.get(i))
+                    .unit(this.ingredientUnit.get(i))
+                    .build();
+            cocktailIngredientRequests.add(build);
+        }
+        return cocktailIngredientRequests;
+    }
 }
