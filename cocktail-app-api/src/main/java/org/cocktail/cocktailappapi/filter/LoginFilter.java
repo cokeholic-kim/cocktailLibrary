5a09b5de92f4c370e34d77d18a8e6b8be2df0136
dong_hy <kimadonghyon99@gmail.com>
2024-11-01 23:09:04 +0900

    refactor : 쿠키 생성함수 수정.

--- cocktail-app-api/src/main/java/org/cocktail/cocktailappapi/filter/LoginFilter.java
+++ cocktail-app-api/src/main/java/org/cocktail/cocktailappapi/filter/LoginFilter.java
@@ -3,11 +3,8 @@ package org.cocktail.cocktailappapi.filter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cocktail.cocktailappapi.domain.user.controller.model.CustomUserDetails;
@@ -18,6 +15,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -83,13 +83,5 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         //인증 실패
         response.setStatus(401);
     }
-
-//    private Cookie createCookie(String key, String value) {
-//
-//        Cookie cookie = new Cookie(key, value);
-//        cookie.setMaxAge(60 * 60 * 1000);
-//        cookie.setPath("/");
-//        return cookie;
-//    }
 }
 
