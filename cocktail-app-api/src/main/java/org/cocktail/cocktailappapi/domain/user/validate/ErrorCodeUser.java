dd351afb99be0dcb4769f180e664819b440627ab
dong_hy <kimadonghyon99@gmail.com>
2024-11-01 23:07:41 +0900

    refactor : 유저 에러 코드 추가.

--- cocktail-app-api/src/main/java/org/cocktail/cocktailappapi/domain/user/validate/ErrorCodeUser.java
+++ cocktail-app-api/src/main/java/org/cocktail/cocktailappapi/domain/user/validate/ErrorCodeUser.java
@@ -6,7 +6,9 @@ public enum ErrorCodeUser implements ErrorCodeIfs {
 
     JOIN_ERROR(400,201,"가입 정보가 잘못입력되었습니다"),
     REDUNDANT_EMAIL(400,202,"사용중인 이메일입니다."),
-    REDUNDANT_NICKNAME(400,203,"사용중인 닉네임 입니다.")
+    REDUNDANT_NICKNAME(400,203,"사용중인 닉네임 입니다."),
+    NULL_USER(400,204,"없는 유저 입니다.")
+
     ;
     private final Integer httpStatusCode;
     private final Integer errorCode;
